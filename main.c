80ae81ba6011b201ee222ff12349a7d76c92c25a
MrBober <53260262+MrBober@users.noreply.github.com>
2022-09-15 16:51:00 +0200

    minor spelling mistake

--- main.c
+++ main.c
@@ -5,23 +5,16 @@
 
 int main()
 {
-	int i = 0;
-	while(1)
-	{
-		i++;
+	for(int i = 1; ; i++) {
 		sleep(1);
-		notify_init ("Another second of being alone!");
+		notify_init("Another second of being alone!");
 
-		char *string;
-		if (i == 1)
-			string = g_strdup_printf("%d second has passed.", i);
-		else
-			string = g_strdup_printf("%d seconds have passed.", i);
+		char *string = g_strdup_printf("%d %s passed.", i, i == 1 ? "second has" : "seconds have");
 
 		NotifyNotification *life = notify_notification_new("Another second of being alone!", string, NULL);
-		notify_notification_set_urgency (life, NOTIFY_URGENCY_CRITICAL);
+		notify_notification_set_urgency(life, NOTIFY_URGENCY_CRITICAL);
 
-		notify_notification_show (life, NULL);
+		notify_notification_show(life, NULL);
 
 		g_object_unref(G_OBJECT(life));
 		notify_uninit();
